9579a94da0bf0817363469ac39b930cec0d48286
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-03-31 22:06:10 +0900

    Thymeleaf and messages(locale)

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
+@Slf4j
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
 public class BasicItemController {
@@ -35,6 +38,7 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm() {
+        log.info("Locale ===> {}", Locale.getDefault());
         return "basic/addForm";
     }
 
