9b6e6f862a064cc4a71a25863d5eda5f28eda0ca
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-04-01 11:46:20 +0900

    Thymeleaf validation check

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,11 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Controller
 @Slf4j
@@ -37,8 +40,8 @@ public class BasicItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
-        log.info("Locale ===> {}", Locale.getDefault());
+    public String addForm(Model model) {
+        model.addAttribute(new Item());
         return "basic/addForm";
     }
 
@@ -74,7 +77,34 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String saveV3(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String saveV3(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+        // 검증에 실패하면 다시 입력 폼으로
+        if(!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            return "basic/addForm";
+        }
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         // {}로 데이터를 넣은게 아니라면 queryParameter로 나머지 attribute가 들어간다.
