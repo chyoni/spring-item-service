5cf3cd70457380d1fbf9a9c40170db3f1d4a8614
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-04-05 21:48:24 +0900

    수정폼과 등록폼에서 사용되는 각자만의 밸리데이션을 처리하는 방법: groups

--- src/main/java/hello/itemservice/web/basic/ValidationItemController2.java
+++ src/main/java/hello/itemservice/web/basic/ValidationItemController2.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.basic;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -46,8 +48,40 @@ public class ValidationItemController2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String save(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // ! 바인딩을 사용하지 않고도 그냥 밸리데이션을 사용할 수 있는데 그게 바로 빈 밸리데이션이다.
+        // ! 그리고 결국 그 Bean Validation도 이전에 배운 BindingResult를 내부적으로 사용해서 에러 코드를 생성하고
+        // ! 그 코드가 있으면 우선순위가 높은 순으로 에러 메시지를 FieldError에 담는 방식으로 구현된다.
+
+        // ! 근데 ObjectError는 Bean Validation으로 처리하기는 좀 애매하기 때문에 이렇게 그냥 자바 코드로 처리하는게 제일 깔끔하다.
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors ={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        // {}로 데이터를 넣은게 아니라면 queryParameter로 나머지 attribute가 들어간다.
+        // ex) localhost:8080/validation/items/3?status=true
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String saveV2(
+            @Validated(value = SaveCheck.class) @ModelAttribute Item item,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
         // ! 바인딩을 사용하지 않고도 그냥 밸리데이션을 사용할 수 있는데 그게 바로 빈 밸리데이션이다.
         // ! 그리고 결국 그 Bean Validation도 이전에 배운 BindingResult를 내부적으로 사용해서 에러 코드를 생성하고
         // ! 그 코드가 있으면 우선순위가 높은 순으로 에러 메시지를 FieldError에 담는 방식으로 구현된다.
@@ -81,8 +115,29 @@ public class ValidationItemController2 {
         return "validation/v2/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
+    //@PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors ={}", bindingResult);
+            return "validation/v2/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        redirectAttributes.addAttribute("itemId", itemId);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editV2(@PathVariable Long itemId, @Validated(value = UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
